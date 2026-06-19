34d504fbdf80976690e0f0109d51885f3889199c
gaurav <gouravkumar42242@gmail.com>
2026-06-19 17:16:57 +0530

    Auth ui

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+import AppRoutes from "./routes/AppRoutes"
+import 'remixicon/fonts/remixicon.css'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
+
+export default App
